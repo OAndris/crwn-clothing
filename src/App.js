15071df7a117224f8ca723c0a01e24964ba96f4e
Andras Orban <orbanandris@gmail.com>
2020-07-09 20:01:20 +0200

    broken our homeage into smaller componennts, finished styling menu-item comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage'
+import HomePage from './pages/homepage/homepage'
 
 function App() {
   return (
-    // <div>Hello world!</div>
     <HomePage />
   );
 }
