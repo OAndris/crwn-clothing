49d689a5be5bb0c6cdc8539f95159e0c6b75bae3
Andras Orban <orbanandris@gmail.com>
2020-10-29 21:09:37 +0100

    Store user data in firebase database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/Shop';
 import SignInAndSignUpPage from './pages/SignInAndSignUp/SignInAndSignUp';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     state = {
@@ -16,8 +16,9 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-            this.setState({ currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+            createUserProfileDocument(user);
+            // this.setState({ currentUser: user });
         });
     }
 
