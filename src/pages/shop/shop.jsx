60a5e73bb4ad196311f110041a36d138a967535b
Andras Orban <orbanandris@gmail.com>
2020-11-06 22:25:26 +0100

    Create separate CollectionsOverview component

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../../components/CollectionPreview/CollectionPreview';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
     <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
+        <CollectionsOverview />
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
