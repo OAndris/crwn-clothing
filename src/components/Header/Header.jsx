7a40855a00938169a267d72899ab4e97ada18154
Andras Orban <orbanandris@gmail.com>
2020-11-08 16:17:54 +0100

    Add uppercase Header folder and files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { auth } from '../../firebase/firebase.utils';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import CartIcon from '../CartIcon/CartIcon';
+import CartDropdown from '../CartDropdown/CartDropdown';
+import './Header.scss';
+
+const Header = ({ currentUser, hidden }) => (
+    <div className="header">
+        <Link className="logo-container" to="/">
+            <Logo className="logo" />
+        </Link>
+        <div className="options">
+            <Link className="option" to="/shop">
+                SHOP
+            </Link>
+            <Link className="option" to="/signin">
+                CONTACT
+            </Link>
+            {currentUser ? (
+                <div className="option" onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
+                <Link className="option" to="/signin">
+                    SIGN IN
+                </Link>
+            )}
+            <CartIcon />
+        </div>
+        {hidden ? null : <CartDropdown />}
+    </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden,
+});
+
+export default connect(mapStateToProps)(Header);
